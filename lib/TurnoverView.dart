a75591d18fe6525a1bf8c857aeec5b13549775b9
CoMatu <matusevichvv@gmail.com>
2018-07-24 17:03:26 +0500

    получение данных из интернета в работе

--- lib/TurnoverView.dart
+++ lib/TurnoverView.dart
@@ -1,18 +1,17 @@
+import 'dart:async';
+
 import 'package:charts_common/src/data/series.dart';
 import 'package:flutter/material.dart';
 import 'package:hr_metrics/SimpleBarChart.dart';
 import 'package:hr_metrics/FetchChartData.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:http/http.dart' as http;
+
 
 
 class TurnoverView extends StatelessWidget{
-  final List<charts.Series> seriesList;
 
-
-
-  const TurnoverView({Key key, this.seriesList}) : super(key: key);
-
-  get fetchChartData => new FetchChartData();
+  const TurnoverView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,18 +21,34 @@ class TurnoverView extends StatelessWidget{
         title: new Text('СРЕДНЯЯ ЗАРПЛАТА'),
 
       ),
-      body: FutureBuilder(
-        future: fetchChartData,
+      body: FutureBuilder<List<charts.Series>>(
+        future: _createSampleData(),
           builder: (context, snapshot){
             if(snapshot.connectionState == ConnectionState.waiting){
               return new Text('Data is loading...');
             }
             else{
-              return new SimpleBarChart(seriesList);
+              return new SimpleBarChart(snapshot);
             }
           },
       ),
     );
   }
+
+     static Future<List<charts.Series<ChartData, String>>> _createSampleData() async {
+      final data = await fetchData(http.Client());
+
+      return [
+        new charts.Series<ChartData, String>(
+            id: 'Sales',
+            domainFn: (ChartData series, _) => series.period,
+            measureFn: (ChartData series, _) => series.count,
+            data: data,
+            labelAccessorFn: (ChartData series, _) => '${series.count.toString()}'
+        )
+      ];
+    }
+
+
 
 }
