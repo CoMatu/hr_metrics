877700a619c05808259e80d6a114fe3058a5142c
CoMatu <matusevichvv@gmail.com>
2018-07-27 17:08:00 +0500

    добавлена popupmenu button

--- lib/SimpleBarChart.dart
+++ lib/SimpleBarChart.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 
 
-class SimpleBarChart extends StatelessWidget {
+class SimpleBarChart extends StatefulWidget {
   final List<charts.Series> seriesList;
   final bool animate;
 
@@ -15,13 +15,57 @@ class SimpleBarChart extends StatelessWidget {
 
 
   @override
+  SimpleBarChartState createState() {
+    return new SimpleBarChartState();
+  }
+
+}
+
+class SimpleBarChartState extends State<SimpleBarChart> {
+  DateTime _time;
+  Map<String, num> _measures;
+
+  // Listens to the underlying selection changes, and updates the information
+  // relevant to building the primitive legend like information under the
+  // chart.
+  _onSelectionChanged(charts.SelectionModel model) {
+    final selectedDatum = model.selectedDatum;
+
+    DateTime time;
+    final measures = <String, num>{};
+
+    // We get the model that updated with a list of [SeriesDatum] which is
+    // simply a pair of series & datum.
+    //
+    // Walk the selection updating the measures map, storing off the sales and
+    // series name for each selection point.
+    if (selectedDatum.isNotEmpty) {
+      time = selectedDatum.first.datum.time;
+      selectedDatum.forEach((charts.SeriesDatum datumPair) {
+        measures[datumPair.series.displayName] = datumPair.datum.sales;
+      });
+    }
+
+    // Request a build.
+    setState(() {
+      _time = time;
+      _measures = measures;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new charts.BarChart(
-      seriesList,
-      animate: animate,
-      barRendererDecorator: new charts.BarLabelDecorator<String>(),
+      widget.seriesList,
+      animate: widget.animate,
+      selectionModels: [
+        new charts.SelectionModelConfig(
+          type: charts.SelectionModelType.info,
+          listener: _onSelectionChanged,
+        )
+      ],
+      //barRendererDecorator: new charts.BarLabelDecorator<String>(),
       vertical: false,
     );
   }
-
 }
