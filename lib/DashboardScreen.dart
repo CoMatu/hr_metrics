4273392aff4caf8dbf41df8e47a898e845b62907
CoMatu <matusevichvv@gmail.com>
2018-08-15 16:57:11 +0500

    десериализация готова

--- lib/DashboardScreen.dart
+++ lib/DashboardScreen.dart
@@ -5,15 +5,14 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:hr_metrics/models/dashboard.dart';
 import 'package:hr_metrics/models/serializers.dart';
 
-
 // экран дашборд с главными показателями
 class DashboardScreen extends StatelessWidget {
   DashboardScreen(this.database);
+
   FirebaseDatabase database;
 
   @override
   Widget build(BuildContext context) {
-
     // TODO: implement build
     return new Scaffold(
       appBar: new AppBar(
@@ -26,37 +25,49 @@ class DashboardScreen extends StatelessWidget {
           padding: const EdgeInsets.all(8.0),
           child: Center(
             child: new FutureBuilder(
-                future:
-                database
+                future: database
                     .reference()
                     .child('dashboardList')
-                    .child('2')
-                    .once()
-                ,
+                    .once(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.data != null) {
                     return _getWidget(snapshot);
+                  } else {
+                    return new CircularProgressIndicator(
+                        valueColor:
+                            new AlwaysStoppedAnimation(Colors.yellow[700]));
                   }
-                     else {
-                      return new CircularProgressIndicator(
-                          valueColor:
-                              new AlwaysStoppedAnimation(Colors.yellow[700]));
-                    }
-
                 }),
           )),
     );
   }
 
   Widget _getWidget(AsyncSnapshot snapshot) {
+    List snapdata = new List();
+    List<Dashboard> dashboardList = new List();
 
-    Map<String, dynamic> data = Map.from(snapshot.data.value);
+    for(var value in snapshot.data.value){
+      snapdata.add(value);
+    }
 
-    Dashboard dashboard = serializers.deserializeWith(Dashboard.serializer,data);
+    for(int i = 0; i< snapdata.length; i++){
+    Map<String, dynamic> data = Map.from(snapdata[i]);
+    Dashboard dashboard =
+    serializers.deserializeWith(Dashboard.serializer, data);
+    dashboardList.add(dashboard);
+    }
 
-    return new Container(
-      child: new Text(dashboard.mainIndicator.toString())
-    )
-    ;
+//    Map<String, dynamic> data = Map.from(snapshot.data.value);
+    var count = dashboardList.length;
+
+    print(count.toString());
+
+    return new Text('В этом List $count значений)');
+/*
+    Dashboard dashboard =
+        serializers.deserializeWith(Dashboard.serializer, data);
+
+    return new Container(child: new Text(dashboard.mainIndicator.toString()));
+*/
   }
 }
